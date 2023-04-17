aaf68ed064893bad891537d947427391ba68bdf5
Deepjyoti Barman <deep.barman30@gmail.com>
2023-04-17 15:27:28 +0530

    Add support to setup temp index if fields are not present

--- metadata/main.py
+++ metadata/main.py
@@ -7,20 +7,18 @@ dataField(s) that the user specifies.
 
 from typing import List, Dict
 from urllib.parse import urljoin
+from re import match
 
 from rich.prompt import Prompt
 from rich import print
-from requests import get
+from requests import get, put
 
 # Following variable should be updated according
 # to the users requirements
 INDEX_URL = "http://localhost:9200/test"  # Index to work on
 FIELDS_TO_ENRICH = ["name"]  # Fields to enrich
-# Suffix to add to the field names to store the enriched data
-#
-# If the field name is `title`, the field metadata will be stored at
-# `title_metadata` if the suffix is set to `_metadata`
-ENRICHED_FIELDS_SUFFIX = "_metadata"
+# Field where the metadata of all the enriched fields will be stored
+ENRICHED_FIELD_NAME = "metadata"
 ENRICH_TIME_FIELD = "metadata_date"  # Field to indicate when enrichment happened
 
 
@@ -183,10 +181,10 @@ def ask_user_inputs():
             "validate_func": validate_fields,
             "parser": parse_fields
         },
-        "enriched_suffix": {
-            "name": "Suffix for enriched fields",
-            "description": "Enter the suffix for enriched fields. If suffix is `_metadata` and field is `test` then final field will be `test_metadata`",
-            "default": ENRICHED_FIELDS_SUFFIX,
+        "enriched_field_name": {
+            "name": "Field name where the metadata will be stored",
+            "description": "Enter the name of the field where the final metadata will be stored",
+            "default": ENRICHED_FIELD_NAME,
             "validate_func": true_validate
         },
         "vector_time_df_name": {
@@ -234,6 +232,113 @@ def ask_user_inputs():
     return values_to_return
 
 
+def get_settings(
+    source_url: str,
+    source_index: str,
+    remove_keys: bool = False
+) -> Dict:
+    """
+    Return the settings and remove pre-existing keys if the flag is passed
+    """
+    settings_url = urljoin(source_url, source_index + "/_settings")
+    response = get(settings_url)
+
+    if not response.ok:
+        raise Exception(
+            "non OK response received while getting settings of source index: ",
+            response.status_code)
+
+    settings_json = response.json().get(source_index,
+                                        {}).get("settings", {}).get("index", {})
+
+    if remove_keys:
+        settings_json.pop("history", None)
+        settings_json.pop("history.uuid", None)
+        settings_json.pop("provided_name", None)
+        settings_json.pop("uuid", None)
+        settings_json.pop("version", None)
+        settings_json.pop("creation_date", None)
+
+    settings_json = {"index": settings_json}
+
+    return settings_json
+
+
+def reindexed_name(index_name: str) -> str:
+    """
+      Generate the re-indexed name of the index that needs to be
+      setup.
+      """
+    is_match = match(".*reindexed_[0-9]+", index_name)
+
+    if not is_match:
+        return index_name + "_reindexed_1"
+
+    # Since the index has _reindexed in it, we will need to extract
+    # the digit and increment it.
+    tokens = index_name.split("_")
+    tokens[-1] = str(int(tokens[-1]) + 1)
+
+    return "_".join(tokens)
+
+
+def enriched_fields_setup(
+    index_url: str,
+    index_name: str,
+    meta_field_name: str,
+    meta_time_field_name: str
+) -> str:
+    """
+    Check if the source index has the metadata field
+    and the other fields required. If it already has it,
+    there is no need to do anything, we can just return
+    the same index's name.
+
+    If the index doesn't have it, we will need to create a
+    temporary index that will follow the reindexed name
+    structure used by RS. This index should have both the
+    metadata fields added along with the settings of the
+    original index.
+
+    This function will return the name of the final index.
+    """
+    source_mappings = get_mapping(index_url, index_name)
+
+    mappings = source_mappings.get(index_name, {}).get(
+        "mappings", {})
+    props = mappings.get("properties", {})
+    prop_keys = props.keys()
+
+    if meta_field_name in prop_keys and meta_time_field_name in prop_keys:
+        # No need for a re-index, both fields are already present.
+        # We can directly return the name of the index.
+        return index_name
+
+    # We will need to create the temporary index and add the
+    temp_index = reindexed_name(index_name)
+
+    # Pull the original settings of the temporary index
+    original_settings = get_settings(index_url, index_name, True)
+
+    create_body = {
+        "mappings": mappings,
+        "settings": original_settings
+    }
+
+    create_index_url = urljoin(index_url, temp_index)
+
+    create_response = put(create_index_url, json=create_body)
+
+    if not create_response.ok:
+        err_msg = "error while creating index, got non OK response code: " + \
+            create_response.status_code
+        print(err_msg)
+        print("response received from create index: ", create_response.json())
+        raise Exception(err_msg)
+
+    return temp_index
+
+
 def main():
     """
     Entry point into the script.
@@ -242,7 +347,31 @@ def main():
     """
     try:
         inputs = ask_user_inputs()
-        print(inputs)
+
+        FIELDS_TO_ENRICH = inputs.get("fields_to_enrich", [])
+        ENRICHED_FIELD_NAME = inputs.get("enriched_field_name", "")
+        ENRICH_TIME_FIELD = inputs.get("enrich_time_field", "")
+
+        source_index_url = inputs.get("cluster_url", "")
+        source_index_name = get_index_alias(
+            source_index_url, inputs.get("index_name", ""))
+
+        # There are two scenarios for the index field:
+        #
+        # - it already has the metadata field mapping
+        # - it doesn't have the metadata field mapping.
+        #
+        # In the first case, we can just go ahead with the indexing
+        # In the second case, we will need to re_index all the data
+        # , delete the older index and then set an alias for the new
+        # index.
+        index_to_work_on = enriched_fields_setup(
+            source_index_url,
+            source_index_name,
+            ENRICHED_FIELD_NAME,
+            ENRICH_TIME_FIELD
+        )
+
     except KeyboardInterrupt:
         # Exit gracefully!
         print("\nInterrupt received, exitting!")
